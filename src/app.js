c0938aaf28aa2aefa5d89620fdd739f14bb6098a
gautamkiaan43-lang <gautamkiaan43@gmail.com>
2026-06-06 12:24:30 +0530

    Update app.js
    k,jj

--- src/app.js
+++ src/app.js
@@ -33,6 +33,7 @@ app.use(cors({
     'http://127.0.0.1:5173',
     'https://restorentposback.softwaredemolive.live',
     'https://gilahouse.com',
+    'https://app.gilahouse.com',
     process.env.FRONTEND_URL
   ].filter(Boolean),
   credentials: true,
